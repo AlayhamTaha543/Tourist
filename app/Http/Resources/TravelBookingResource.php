e32e151f70fc86e33dd18407f696aa93aedf1edb
tahaalayham <alayhamt795@gmail.com>
2025-08-30 09:19:10 +0300

    fix(TravelBookingResource): replace null defaults with 'N/A' for flight details
    Ensure consistent display of flight information by providing 'N/A' as default values instead of null. This improves user experience by clearly indicating missing data.

--- app/Http/Resources/TravelBookingResource.php
+++ app/Http/Resources/TravelBookingResource.php
@@ -19,9 +19,9 @@ class TravelBookingResource extends JsonResource
             'date' => $this->booking_date,
             'total_price' => $this->total_price,
             'flight' => [
-                'flight_number' => optional(optional($this->travelBooking)->flight)->flight_number ?? null,
-                'departure_location' => optional(optional(optional($this->travelBooking)->flight)->departure)->name ?? null,
-                'arrival_location' => optional(optional(optional($this->travelBooking)->flight)->arrival)->name ?? null,
+                'flight_number' => optional(optional($this->travelBooking)->flight)->flight_number ?? 'N/A',
+                'departure_location' => optional(optional(optional($this->travelBooking)->flight)->departure)->city->country->code ?? 'N/A',
+                'arrival_location' => optional(optional(optional($this->travelBooking)->flight)->arrival)->city->country->code ?? 'N/A',
             ],
             'number_of_people' => optional($this->travelBooking)->number_of_people ?? 0,
         ];
